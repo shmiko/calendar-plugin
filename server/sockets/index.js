93c464b91e7049d582dfc2415bfc50f635654656
Mathieu "ralmn" HIREL <contact.ralmn@gmail.com>
2015-11-14 22:12:49 +0100

    Move sockets into sockets/index.js

---
+++ server/sockets/index.js
@@ -0,0 +1,35 @@
+var Calendar = require('../models/calendar.js');
+var google = require('googleapis');
+var googleAuth = require('google-auth-library');
+
+
+module.exports = function(socket){
+  
+  socket.on('calendar/list', function(callback){
+    Calendar.find({user:socket.request.user}, callback);
+  });
+
+  socket.on('calendar/get', function(id, callback){
+    Calendar.findById(id, callback);
+  });
+
+  socket.on('calendar/delete', function(calendarP, callback){
+    Calendar.findOneAndRemove({_id:calendarP._id, user:socket.request.user}, callback);
+  });
+
+  socket.on('calendar/save', function(calendar, callback){
+    if(calendar._id == null){
+      calendar.user = socket.request.user;
+      Calendar.create(calendar, callback);
+    }else{
+      var id = calendar._id
+      delete calendar._id;
+      Calendar.update({_id:id}, calendar, {}, callback);
+    }
+  });
+
+  require('./google')(socket);
+
+
+
+}
